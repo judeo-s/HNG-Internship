b5ea8de7ab0b2ee3f26f4fd413ea0e636f6c9028
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2025-01-30 13:40:32 +0000

    :sparkles: included CORS in he API

--- backend/stage0/main.py
+++ backend/stage0/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 from pydantic import BaseModel
 from typing import Any
+
 """
 Simple api for HNG Internship Stage 0
 """
@@ -14,12 +16,20 @@ class IndexResponse(BaseModel):
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get('/', response_model=IndexResponse, status_code=status.HTTP_200_OK)
 async def index() -> Any:
     """Index route for HNG Internship Stage 0"""
     return IndexResponse(
             email="osamsackeyjude@gmail.com",
-            current_datetime= datetime.now().replace(microsecond=0).isoformat(),
+            current_datetime=datetime.now().replace(microsecond=0).isoformat(),
             github_url="https://github.com/judeo-s/HNG-Internship/tree/main"
         )
